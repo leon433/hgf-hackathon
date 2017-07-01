653da4da6acebb8c69c5454ed4b80a25a9f85381
Leon <lzhang3011@gmail.com>
2017-07-01 15:48:00 +0100

    Working on first patentee page

--- hgf_app/views.py
+++ hgf_app/views.py
@@ -1,6 +1,14 @@
 from hgf_app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'testing'
+
+	return render_template('index.html')
+
+@app.route('/patentee1')
+def patentee1():
+
+	return render_template('patentee1.html')
